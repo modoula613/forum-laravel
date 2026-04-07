f534d54e04e164ae3d26fb24dbe45743bc87bc4a
Mohamed-Abdullah El bouayadi <modoula.elbou@MacBook-Air-de-Mohamed-Abdullah.local>
2026-04-07 10:48:54 +0200

    Refine compose icon as feather drawing

--- resources/views/components/compose-feather-icon.blade.php
+++ resources/views/components/compose-feather-icon.blade.php
@@ -1,24 +1,54 @@
 @props(['class' => 'h-5 w-5'])
 
-<svg {{ $attributes->merge(['class' => $class]) }} viewBox="0 0 24 24" fill="none" aria-hidden="true">
+<svg {{ $attributes->merge(['class' => $class]) }} viewBox="0 0 32 32" fill="none" aria-hidden="true">
     <path
-        d="M20.4 3.6c-6 .5-10.8 5.3-11.3 11.3L8.1 20l5.1-1c6-.5 10.8-5.3 11.3-11.3l.3-3.1a.75.75 0 0 0-.82-.82l-3.58.32Z"
+        d="M7.5 22.5c0-1.3 1-2.3 2.3-2.3h8.6c1.3 0 2.3 1 2.3 2.3v4.1c0 1.5-1.2 2.7-2.7 2.7h-7.8c-1.5 0-2.7-1.2-2.7-2.7Z"
+        stroke="currentColor"
+        stroke-width="1.8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M7 22.8c0-.8.7-1.5 1.5-1.5h11.2c.8 0 1.5.7 1.5 1.5"
+        stroke="currentColor"
+        stroke-width="1.8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M10.8 20.2c.2-2.6 1.1-4.8 2.7-6.7"
         stroke="currentColor"
         stroke-width="1.65"
         stroke-linecap="round"
         stroke-linejoin="round"
     />
     <path
-        d="M9.5 14.5 20.3 3.7"
+        d="M13.3 13.7c.9-6 5.8-10 11.2-10.9.5-.1.9.3.9.8-.1 4.6-1.9 9.9-6.9 12.4-1.5.8-3.1 1.1-4.7 1.2"
         stroke="currentColor"
-        stroke-width="1.65"
+        stroke-width="1.8"
         stroke-linecap="round"
         stroke-linejoin="round"
     />
     <path
-        d="m8.55 16.15 3.1.6M8.95 13.15l4.45.85M10.4 10.2l5.2 1M12.4 7.35l5 1"
+        d="M13.5 14.4 23 5.1"
         stroke="currentColor"
-        stroke-width="1.45"
+        stroke-width="1.55"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="m16.1 11.4 2.7-.5m-4.1 3.2 3-.2m-1.2-6.1 3.3-.4m-.8 4.1 2.8-.5m-1.8-5.7 2.9-.3"
+        stroke="currentColor"
+        stroke-opacity=".72"
+        stroke-width="1.2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="m12.9 15.8 2.5 1m-3.7.4 2.8 1m-.4 7.3h4.3m-8.3-1.9h3.2"
+        stroke="currentColor"
+        stroke-opacity=".6"
+        stroke-width="1.15"
         stroke-linecap="round"
         stroke-linejoin="round"
     />
